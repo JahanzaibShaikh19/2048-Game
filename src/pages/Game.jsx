d8f928677552109db2a27ed7db68456fa60fbf3f
unknown <jahanzaibshaikh16@gmail.com>
2024-07-18 17:28:51 +0500

    set features

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -171,8 +171,8 @@ const Game = () => {
         return 'bg-gray-700';
       case 4:
         return 'bg-cyan-600';
-      // case 8:
-      //   return 'bg-orange-700';
+      case 8:
+        return 'bg-orange-700';
       // case 16:
       //   return 'bg-yellow-700';
       // case 32:
