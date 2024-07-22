9b4f1580579f937a60564c60c1aaa1eb43905810
unknown <jahanzaibshaikh16@gmail.com>
2024-07-22 21:27:18 +0500

    Added Footer

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -2,7 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import App from "../App";
 import Game from "../pages/Game";
 import Navbar from "../components/ui/Navbar";
-
+import Footer from "../components/ui/Footer";
 const AppRouter = () => {
   return (
     <>
@@ -12,6 +12,7 @@ const AppRouter = () => {
         <Route path="/*" element={<App />} />
         <Route path="/game" element={<Game />} />
       </Routes>
+      <Footer/>
     </>
   );
 };
