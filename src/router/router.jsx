f955ef7cee6f39667a413200558ab75f6349321d
unknown <jahanzaibshaikh16@gmail.com>
2024-07-22 21:37:33 +0500

    404 Error Page Added

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -3,11 +3,14 @@ import App from "../App";
 import Game from "../pages/Game";
 import Navbar from "../components/ui/Navbar";
 import Footer from "../components/ui/Footer";
+import PageNotFound from "../pages/404ErrorPage";
+
 const AppRouter = () => {
   return (
     <>
       <Navbar />
       <Routes>
+        <Route path="*" element={<PageNotFound />} />
         <Route path="/" element={<App />} />
         <Route path="/*" element={<App />} />
         <Route path="/game" element={<Game />} />
